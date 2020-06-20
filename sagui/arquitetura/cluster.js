59c3c236731a3c92d23bb1d53f8c8b46f489e7ef
Ronaldo Manfredini <ronald677br@gmail.com>
2020-06-20 15:04:25 -0300

    almost arq

--- sagui/arquitetura/cluster.js
+++ sagui/arquitetura/cluster.js
@@ -4,22 +4,48 @@ const {
 const { BaseMessage, MessageTypes } = require('./messageInterface.js');
 
 const args = process.argv.slice(2);
-const workerCount = args[0];
+const workerCount = parseInt(args[0]);
+const initialSender = parseInt(args[1])
+
+var initialMessage = new BaseMessage();
+initialMessage.type = MessageTypes.TRANSFER;
+initialMessage.message = args[3];
+initialMessage.originalSender = initialSender;
+initialMessage.actualSender = initialSender;
+initialMessage.target = parseInt(args[2]);
+
 var workers = [];
 var messageChannels = [];
 
+function getDirection(actual, target) {
+    let rightPath = Math.abs(actual -  target);
+    let leftPath  = workerCount - target + actual;
+    if (leftPath > workerCount) {
+        leftPath = workerCount - (leftPath % workerCount);
+    }
+    console.log(leftPath, rightPath);
+    return rightPath <= leftPath? 1: -1;
+}
+
+function resolveSender(actual, target) {
+    let nextSender = actual + getDirection(actual, target);
+    if (nextSender == 0) return workerCount;
+    if (nextSender == workerCount + 1) return 1;
+    return nextSender;
+}
+
 function handleTransferMessage(data) {
-    let routeLeft = data.actualSender;
+    workers[resolveSender(data.actualSender, data.target)].postMessage(data);
 }
 
 function handleMessage(messageObj) {
     if (messageObj.type == MessageTypes.SUCCESS) console.log(messageObj.message);
-    if (message.type == MessageTypes.TRANSFER) handleTransferMessage(messageObj);
+    if (messageObj.type == MessageTypes.TRANSFER) handleTransferMessage(messageObj);
 }
 
 
 for (let i = 1; i <= workerCount; i++) {
-    workers[i] = new Worker('./worker.js', {argv: [i]});
+    workers[i] = new Worker("./worker.js", {argv: [i]});
     messageChannels[i] = new MessageChannel();
 
     let setupMessage = new BaseMessage();
@@ -30,4 +56,4 @@ for (let i = 1; i <= workerCount; i++) {
     messageChannels[i].port2.on("message", handleMessage);
 }
 
-
+workers[initialSender].postMessage(initialMessage);
--- sagui/arquitetura/worker.js
+++ sagui/arquitetura/worker.js
@@ -1,4 +1,4 @@
-const { BaseMessage, MessageTypes } = require('./messageInterface.js');
+const { MessageTypes } = require('./messageInterface.js');
 const { parentPort } = require('worker_threads');
 
 const args = process.argv.slice(2);
@@ -10,27 +10,18 @@ function setupWorker(data) {
 }
 
 function handleTransferMessage(data) {
-    let messageToSend = new BaseMessage();
-    messageToSend.target = data.target;
-    messageToSend.originalSender = data.originalSender;
-    messageToSend.message = data.message;
-    messageToSend.actualSender = workerId;
-
-    if (data.message == workerId) {
-        messageToSend.type = MessageTypes.SUCCESS;
-        messageToSend.message = `Worker ${workerId} recebeu com sucesso a mensagem ${data.message}, vinda do Worker ${data.originalSender}`;
-        messagePort.postMessage(messageToSend);
-    } else {
-        messageToSend.type = MessageTypes.TRANSFER;
-        messagePort.postMessage(messageToSend);
+    console.log(`Worker ${workerId} recebeu com sucesso a mensagem ${data.message}, vinda do Worker ${data.actualSender} com destino ao Worker ${data.target}`);
+    data.actualSender = workerId;
+    if (data.target == workerId) {
+        data.type = MessageTypes.SUCCESS;
+        data.message = `Mensagem chegou ao destino`;
     }
+    messagePort.postMessage(data);
 }
 
 function handleMessage(messageObj) {
     if (messageObj.type == MessageTypes.SETUP && messagePort === undefined) setupWorker(messageObj);
-    else if (messageObj.type == MessageTypes.TRANSFER) handleTransferMessage(messageObj);
+    else if (messageObj.type == MessageTypes.TRANSFER){ handleTransferMessage(messageObj); };
 }
 
-
-
-parentPort.once("message", handleMessage);
+parentPort.on("message", handleMessage);
