b1000555edd37d14d463c2a57c8fbc520919f6e0
Ronaldo Manfredini <ronamanfredini@gmail.com>
2024-10-15 07:56:19 -0300

    feat: generate parentheses init

---
+++ neetcode/stacks/generate-parentheses.py
@@ -0,0 +1,7 @@
+# URL - https://neetcode.io/problems/generate-parentheses
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        pass
+
+s = Solution()
+print(s.generateParenthesis(1))
