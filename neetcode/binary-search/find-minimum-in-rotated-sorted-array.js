e303f3180fb8357e50b60993a9c26bbba1336949
Ronaldo Manfredini <ronamanfredini@gmail.com>
2024-10-22 08:00:57 -0300

    feat: more problems

---
+++ neetcode/binary-search/find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,15 @@
+// URL - https://neetcode.io/problems/find-minimum-in-rotated-sorted-array
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        
+    }
+}
+
+const s = new Solution();
+const nums = [3,4,5,6,1,2];
+console.log(s.findMin(nums))
