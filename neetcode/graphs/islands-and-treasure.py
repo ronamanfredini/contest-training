e1894ea4f4b0042eeddd67e90013af3d6de472ee
Ronaldo Manfredini <ronamanfredini@gmail.com>
2024-12-26 09:24:20 +0100

    feat: more tests

---
+++ neetcode/graphs/islands-and-treasure.py
@@ -0,0 +1,19 @@
+# https://neetcode.io/problems/islands-and-treasure
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        memo = set()
+
+        def dfs(i, j):
+            
+        
+        
+
+s = Solution()
+g = [
+  [2147483647,-1,0,2147483647],
+  [2147483647,2147483647,2147483647,-1],
+  [2147483647,-1,2147483647,-1],
+  [0,-1,2147483647,2147483647]
+]
+s.islandsAndTreasure(g)
