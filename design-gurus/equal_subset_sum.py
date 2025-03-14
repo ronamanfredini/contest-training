6f3dfa0d6a0edfe051f178b91aa9f4b0d808bf57
Ronaldo Manfredini <ronamanfredini@gmail.com>
2025-03-14 08:43:13 -0300

    more problems

---
+++ design-gurus/equal_subset_sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTargetSubsets(self, num, s):
+        unique_sets = set()
+        target_sum_reached_count = 0
+
+        def dfs(i = 0, current_set = []):
+            if i >= len(num):
+                return
+            
+            
+            
+    
+
+
+
+s = Solution()
+print(s.findTargetSubsets([1, 1, 2, 3], 4))
