249e5b8ee1a632e9f789293e129f55a8a2fe8457
Ronaldo Manfredini <ronamanfredini@gmail.com>
2025-04-16 09:30:49 -0300

    feat: more problems

--- design-gurus/palindrome-partitioning-2nd-attempt.py
+++ design-gurus/palindrome-partitioning-2nd-attempt.py
@@ -13,8 +13,17 @@ class Solution:
             return True
 
         def dfs(s = s):
+            if s in memo:
+                return memo.get(s)
+            if is_palindrome(s):
+                return 0
+            min_cuts = len(s) - 1
+            for i in range(len(s)):
+                if is_palindrome(s[:i]):
+                    min_cuts = min(min_cuts, 1 + dfs(s[i + 1:]))
             
-
+            memo[s] = min_cuts
+            return min_cuts
 
         return dfs(s)
 
